9d59df2142080a37ddbd81c466bd92a839f6aa7e
Ahmed Rabie <ahmedrabie54524@gmail.com>
2023-09-10 20:35:55 +0300

    advanced-to-do-list-typescript-vite

---
+++ to-do-list/src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="bg-red-500 text-center">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
